8421d50fc65a5d9c168f1af87de06982552255ea
Mehar <meharmailbox@gmail.com>
2018-03-21 23:51:07 +0530

    first commit

---
+++ app/src/main/java/jamb/com/snu_course_guide/MainActivity.java
@@ -0,0 +1,13 @@
+package jamb.com.snu_course_guide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
